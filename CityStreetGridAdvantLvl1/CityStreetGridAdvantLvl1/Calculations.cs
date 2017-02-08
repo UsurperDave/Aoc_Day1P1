f8f5781874624c0930e51cb7590a81580a09b0bc
UsurperDave <David.Barnes@WoltersKluwer.com>
2017-02-08 14:14:46 -0600

    Day One: TaxiCab
    Find the route based on input.

---
+++ CityStreetGridAdvantLvl1/CityStreetGridAdvantLvl1/Calculations.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CityStreetGridAdvantLvl1
+{
+    public struct currentLocation
+    {
+        // I am going to create a struct for EVERY block that I traverse through.
+        // Example: Entry R4 would create 8 structs of (0,1),(0,2),(0,3),(0,4)
+        // Before I create and add a new location, I need to check that I haven't been here before.
+        // If I have been here, what is the distance?
+         
+        public int _x;
+        public int _y;
+        public int _distance;
+
+        public currentLocation(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            _distance = Math.Abs(x) + Math.Abs(y);            
+        }
+    }
+
+    class Calculations
+    {
+        int _distanceFromHQ = 0;
+        int _prevX = 0;
+        int _prevY = 0;
+
+        public int DistanceFromHQ { set{_distanceFromHQ = value;} }
+        
+        List<currentLocation> locations = new List<currentLocation>();
+
+        public void AddLocationX(int x) // 1 -3 = -2 = (0, -1, -2)
+        {
+            bool result = false;
+            currentLocation cl;
+
+            int currentX = _prevX;
+            int moveTO = currentX + x;
+
+            for(int i = 0; i < Math.Abs(x); i++)
+            {                
+                if(x >= 0)
+                    cl = new currentLocation( (currentX + x) - (x - (i+1)) , _prevY);
+                else
+                    cl = new currentLocation((currentX + x) + Math.Abs((x + (i+1))), _prevY);
+
+                foreach(currentLocation loc in locations)
+                {                                        
+                    if(loc.Equals(cl))
+                    {
+                        result = true;
+                        MessageBox.Show("Distance: " + cl._distance.ToString());                        
+                    }
+                }
+
+                if (result == false)
+                { 
+                    _distanceFromHQ = cl._distance;
+                    locations.Add(cl);
+                }
+            }
+
+            _prevX += x;
+        }
+
+        public void AddLocationY(int y)
+        {
+            bool result = false;
+            currentLocation cl;
+            int currentY = _prevY;
+
+            for (int i = 0; i < Math.Abs(y); i++)
+            {
+                if (y >= 0)
+                    cl = new currentLocation( _prevX, (currentY + y) - (y - (i + 1)));
+                else
+                    cl = new currentLocation( _prevX, (currentY + y) + Math.Abs((y + (i+1))));
+
+                foreach(currentLocation loc in locations)
+                {                                        
+                    if(loc.Equals(cl) == true)
+                    {
+                        result = true;
+                        MessageBox.Show("Distance: " + cl._distance.ToString());                        
+                    }
+                }
+
+                if (result == false)
+                {                    
+                    _distanceFromHQ = cl._distance;
+                    locations.Add(cl);
+                }
+            }
+            _prevY += y;
+        }               
+
+    }
+}
---
+++ CityStreetGridAdvantLvl1/CityStreetGridAdvantLvl1/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace CityStreetGridAdvantLvl1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> movementList = new List<string>();
+
+        public enum Directon
+        {
+            North, 
+            East, 
+            West, 
+            South
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile(movementList);
+        }
+
+        private void CalculateDirections(List<string> movementList)
+        {
+            int distance = 0;
+            int x = 0;
+            int y = 0;
+
+            Directon oldDir = Directon.North;
+            Directon newDir;
+
+            Calculations calc = new Calculations();
+
+            foreach(string move in movementList)
+            {
+                newDir = DetermineDirection(oldDir, Convert.ToChar(move.Substring(0,1))); // Returns direction facing (N,E,W,S)
+
+                x = 0;
+                y = 0;
+
+                switch(newDir)
+                {
+                    case Directon.North:
+                            y += Convert.ToInt32(move.Substring(1));
+                            calc.AddLocationY(y);
+                        break;
+
+                    case Directon.East :
+                            x -= Convert.ToInt32(move.Substring(1));
+                            calc.AddLocationX(x);
+                        break;
+
+                    case Directon.West :
+                            x += Convert.ToInt32(move.Substring(1));
+                            calc.AddLocationX(x);
+                        break;
+
+                    case Directon.South :
+                            y -= Convert.ToInt32(move.Substring(1));
+                            calc.AddLocationY(y);
+                        break;
+                    default :
+                        break;
+                }
+
+                
+                oldDir = newDir;
+            }
+
+            
+            distance = Math.Abs(x) + Math.Abs(y);
+            tbX.Text = x.ToString();
+            tbY.Text = y.ToString();
+            tbDistance.Text = distance.ToString();
+        }
+
+        private Directon DetermineDirection(Directon oldDirection, char direction)
+        {
+            Directon directionFacing = Directon.North;
+            
+            if (oldDirection == Directon.North)
+            {
+                if (direction == 'R')
+                    directionFacing = Directon.West;
+                else if (direction == 'L')
+                    directionFacing = Directon.East;
+            }
+
+            if (oldDirection == Directon.East)
+            {
+                if (direction == 'R')
+                    directionFacing = Directon.North;
+                else if (direction == 'L')
+                    directionFacing = Directon.South;
+            }
+
+            if (oldDirection == Directon.West)
+            {
+                if (direction == 'R')
+                    directionFacing = Directon.South;
+                else if (direction == 'L')
+                    directionFacing = Directon.North;
+            }
+
+            if (oldDirection == Directon.South)
+            {
+                if (direction == 'R')
+                    directionFacing = Directon.East;
+                else if (direction == 'L')
+                    directionFacing = Directon.West;
+            }
+
+            return directionFacing;            
+        }
+
+        private void OpenFile(List<string> movements)
+        {
+            StreamReader sr = null;
+
+            try
+            {
+                sr = new StreamReader(tbFileName.Text.ToString());
+                
+                string directions = sr.ReadToEnd();
+
+                movements.AddRange(directions.Split(',').Select(p => p.Trim()).ToList());
+
+                lbDirections.ItemsSource = movements;
+
+                tbCount.Text = movements.Count().ToString();
+            }
+
+            catch(FileNotFoundException ex)
+            {
+                MessageBox.Show("File not found.  " + ex.Message);
+            }
+
+            catch(UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("You are not authorized to access this file.  " + ex.Message);
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error occured trying to open or read file.  " + ex.Message);
+            }
+
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+            }
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {   
+            CalculateDirections(movementList);
+        }
+    }
+}
